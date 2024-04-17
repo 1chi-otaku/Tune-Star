5346daf4988d4bce55584f4b9077aac6bcbff85c
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-04-17 17:49:48 +0100

    DTO begin

---
+++ Tune Star.BLL/Interfaces/IUserService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tune_Star.BLL.DTO;
+
+namespace Tune_Star.BLL.Interfaces
+{
+    public interface IUserService
+    {
+        Task CreateUser(UserDTO userDto);
+        Task UpdateUser(UserDTO userDto);
+        Task DeleteUser(int id);
+        Task<UserDTO> GetUser(int id);
+        Task<IEnumerable<UserDTO>> GetUsers();
+    }
+}
