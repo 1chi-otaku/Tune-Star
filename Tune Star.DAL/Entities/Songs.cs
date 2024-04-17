d0a66d40a37740dc5d38517c3f954a9661549d21
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-04-17 17:16:06 +0100

    Entities added

---
+++ Tune Star.DAL/Entities/Songs.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tune_Star.DAL.Entities
+{
+    public class Songs
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Artist { get; set; }
+        public string? Img { get; set; }
+        public int GenreId { get; set; }
+        public Genres? Genre { get; set; }
+
+
+
+    }
+}
