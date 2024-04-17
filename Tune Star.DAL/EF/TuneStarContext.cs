8afd1bba7ff3fc75ea906ed4722e9170a7584545
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-04-17 17:33:08 +0100

    DAL done.

---
+++ Tune Star.DAL/EF/TuneStarContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using Tune_Star.DAL.Entities;
+
+namespace Tune_Star.DAL.EF
+{
+    public class TuneStarContext : DbContext
+    {
+        public DbSet<Songs> Songs { get; set; }
+        public DbSet<Genres> Genres { get; set; }
+        public DbSet<Users> Users { get; set; }
+        public TuneStarContext(DbContextOptions<TuneStarContext> options)
+                   : base(options)
+        {
+            Database.EnsureCreated();
+        }
+    }
+}
---
+++ Tune Star.DAL/Repositories/GenresRepository.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tune_Star.DAL.EF;
+using Tune_Star.DAL.Entities;
+using Tune_Star.DAL.Interfaces;
+
+namespace Tune_Star.DAL.Repositories
+{
+    public class GenresRepository : IRepository<Genres>
+    {
+        private TuneStarContext db;
+
+        public GenresRepository(TuneStarContext context)
+        {
+            this.db = context;
+        }
+
+        public async Task<IEnumerable<Genres>> GetAll()
+        {
+            return await db.Genres.ToListAsync();
+        }
+
+        public async Task<Genres> Get(int id)
+        {
+            Genres? team = await db.Genres.FindAsync(id);
+            return team!;
+        }
+
+        public async Task<Genres> Get(string name)
+        {
+            var teams = await db.Genres.Where(a => a.Name == name).ToListAsync();
+            Genres? team = teams?.FirstOrDefault();
+            return team!;
+        }
+
+        public async Task Create(Genres team)
+        {
+            await db.Genres.AddAsync(team);
+        }
+
+        public void Update(Genres team)
+        {
+            db.Entry(team).State = EntityState.Modified;
+        }
+
+        public async Task Delete(int id)
+        {
+            Genres? team = await db.Genres.FindAsync(id);
+            if (team != null)
+                db.Genres.Remove(team);
+        }
+    }
+}
