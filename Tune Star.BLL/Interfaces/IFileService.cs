df41ac4f416f39547a3a05dee7137f1a490640f3
Ichi <89454695+1chi-otaku@users.noreply.github.com>
2024-05-03 11:51:14 +0100

    View Models Added

---
+++ Tune Star.BLL/Interfaces/IFileService.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+
+
+namespace Tune_Star.BLL.Interfaces
+{
+    public interface IFileService
+    {
+        Task<string> SaveImageAsync(IFormFile file, string uploadFolder);
+        Task<string> SaveAudioAsync(IFormFile file, string uploadFolder);
+    }
+}
+
---
+++ Tune Star/Models/FilterViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Tune_Star.BLL.DTO;
+
+namespace Tune_Star.Models
+{
+    public class FilterViewModel
+    {
+        public FilterViewModel(List<GenreDTO> genres, int genre, string position)
+        {
+            genres.Insert(0, new GenreDTO { Name = "All", Id = 0 });
+            Genres = new SelectList(genres, "Id", "Name", genre);
+            SelectedGenre = genre;
+            SelectedPosition = position;
+        }
+        public SelectList Genres { get; }
+        public int SelectedGenre { get; }
+        public string SelectedPosition { get; }
+    }
+}
